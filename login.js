6da54ca1a01a37274e5e3ac103ed26678658f0ff
Zak_Ki <cheweyzak002@gmail.com>
2017-02-15 17:26:54 +1300

    started mysql query for make user session

--- login.js
+++ login.js
@@ -7,10 +7,11 @@ var md5   = require('js-md5')
 exports.handler = function(event, context, callback) {
   function makeCookieData() {
   var date=new Date();
-  date.setTime(+ date + (10)); //24 \* 60 \* 60 \* 100
+  date.setTime(+ date + 60*60*24*process.env.CookieTTL); //24 \* 60 \* 60 \* 100
   var cookieVal = Math.random().toString(36).substring(7); // Generate a random cookie string
   //var cookieString = "myCookie="+cookieVal+"; domain=micloud; expires="+date.toGMTString()+";";
   var cookieString = "sessionId="+cookieVal
+  return {cookieString,cookieVal,ttl:date.toGMTString()}
 }
   
   var connection = mysql.createConnection({
@@ -62,13 +63,15 @@ if (getCookie('sessionId')!=0){
 }
 
 function logMeIn() {
-    connection.query('SELECT  userid, password, firstName, lastName, organisation FROM Micloud.users WHERE username=?',event.body.username,function(error, results, fields){
+    connection.query('SELECT  userid, password, firstName, lastName, organisation FROM Micloud.users WHERE username=?',event.body.username,(error, results, fields)=>{
       if (results[0]){
         let md5Password=md5(event.body.password)
         if (md5Password==results[0].password){
           //make login session
           //context.done(null,{"passwordMatch":"yes"})
-          
+          let inserts=[makeCookieData().cookieVal,results[0].userId,]
+          connection.query('INSERT INTO sessions (sessionId, userId, expiry, userAgent) VALUES(?, ?, NOW() + INTERVAL 7 DAY, ?);')
+          //context.done(null,{'Content':makeCookieData().cookieVal})
           
         }else{
           //Invalid password
